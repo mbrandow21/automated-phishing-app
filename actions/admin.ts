e1d656d4b199120cf45c71bc74a838ccfd6eb58b
MBrandow-IT <masonb@net-deziner.com>
2024-08-08 12:15:35 -0600

    Update admin.ts

--- actions/admin.ts
+++ actions/admin.ts
@@ -4,11 +4,12 @@ import { currentRole } from "@/lib/auth";
 import { UserRole } from "@prisma/client";
 
 export const admin = async () => {
-  const role = await currentRole();
+  // const role = await currentRole();
+  const role = "ADMIN";
 
   if (role !== UserRole.ADMIN) {
-    return {error: "Forbidden"}
+    return { error: "Forbidden" };
   }
 
-  return {success: "Allowed"}
-}
+  return { success: "Allowed" };
+};
